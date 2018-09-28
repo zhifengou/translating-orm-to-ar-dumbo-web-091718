03467e6b878b1b33cb2cd2aa48ecc5034f9eaa47
zhifeng ou <zhifengou@hotmail.com>
2018-09-28 17:37:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,29 @@
-class Dog
+class Dog<ActiveRecord::Base
+  def self.create
+    sql=<<-SQL
+    CREATE TABLE dogs (id INTEGER PRIMARY KEY,name TEXT,breed TEXT)
+    SQL
+    # ActiveRecord::Base.connection.execute(sql)
+  end
+  
+  def save 
+  
+end 
+
+  def update 
+  
+end 
+  
+  def find_or_create_by
+  
 end
+  
+  def find_by_id
+  
+end
+
+  def find_by_name 
+end 
+
+end 
+  
